5cc3a27d14ac5aac4b4da75fd20bbd710328db37
light <pch19980807@gmail.com>
2020-08-22 16:23:23 +0800

    接收部分语法完工

--- UserGUI.py
+++ UserGUI.py
@@ -2,36 +2,88 @@ import sys
 from PyQt5 import uic
 import serial
 import serial.tools.list_ports
-
+from threading import Thread,Lock
+#from PySide2.QtCore import Signal,QObject
+#from PyQt5.QtCore import QObject
+import time
+from PyQt5.QtCore import *
+OpenSerSingal=0
+class MySignals(QObject):
+    UartPrintStr = pyqtSignal(str)
 class MainGUI:
-
     def __init__(self):
         # 从文件中加载UI定义
         MainGUI.ui = uic.loadUi("UserUI/RobotUI.ui")
         MainGUI.ui.ReFlashUart.clicked.connect(MainGUI.ReFlashUart)
         MainGUI.ui.OpenUart.clicked.connect(MainGUI.OpenSerial)
         MainGUI.ui.UartSendButtom.clicked.connect(MainGUI.SendMessage)
+
+        MainGUI.UartPrint=MySignals()
+        MainGUI.UartPrint.UartPrintStr.connect(MainGUI.UartPrintGUI)
     def ReFlashUart(self):
         global PortList
         MainGUI.ui.UartList.clear()
         PortList = list(serial.tools.list_ports.comports())
         for i in list(PortList) :
             MainGUI.ui.UartList.addItem(i[1])
-            
+    def UartPrintGUI(self,text):
+        MainGUI.ui.UartRec.append=(str(text))        
     def OpenSerial(self):
-        global PortList,SlaveSer
-        for i in list(PortList):
-            if MainGUI.ui.UartList.currentText() == i[1] :
-                try:
-                    SlaveSer=serial.Serial(i[0],115200,timeout=60)
-                    #SlaveSer=serial.Serial("/dev/ttyACM0",115200,timeout=60)
-                except Exception:
-                    print("发生了什么错误qaq")
-                if SlaveSer.isOpen() :
-                    MainGUI.ui.UartStates.setText(i[1]+"已经打开")
-                else :
-                    MainGUI.ui.UartStates.setText(i[1]+"未打开")
+        global PortList,SlaveSer,OpenSerSingal
+        if  OpenSerSingal==0 :
+            for i in list(PortList):
+                if MainGUI.ui.UartList.currentText() == i[1] :
+                    try:
+                        SlaveSer=serial.Serial(i[0],115200,timeout=60)
+                        #SlaveSer=serial.Serial("/dev/ttyACM0",115200,timeout=60)
+                    except Exception:
+                        print("发生了什么错误qaq")
+                    if SlaveSer.isOpen() :
+                        MainGUI.ui.UartStates.setText(i[1]+"已经打开")
+                        OpenSerSingal=1
+                    else :
+                        MainGUI.ui.UartStates.setText(i[1]+"未打开")
+                        OpenSerSingal=0
+        return SlaveSer
         #SlaveSer.write("123456".encode('utf-8'))
+    def UartRecBegin(self):
+        global lock
+        def Run(self):
+            global lock
+            ser=self.OpenSerial()
+            while True:
+                lock.acquire()
+                if ser.isOpen():
+                    count = ser.inWaiting()
+                    if count!=0:
+                        UartStr=ser.read(count)
+                    else :
+                        print("串口没开")
+                    self.UartPrint.UartPrintStr.emit(UartStr)
+                    SlaveSer.flushInput()
+                lock.release()
+        thread = Thread(target = self.Run)
+        lock = Lock()
+        thread.start()
+
+
     def SendMessage(self):
         global SlaveSer
         SlaveSer.write(MainGUI.ui.UartSend.toPlainText().encode('utf-8'))
+""" class UartRecThread(QThread):
+    _single=pyqtSignal(str)
+    def __init__(self,ser):
+        super(MyThread,self).__init__()
+        self.ser=SlaveSer
+ 
+    def run(self):
+        while True:
+            try:
+                while self.ser!=None and self.ser.is_open==True:
+                    count=self.ser.in_waiting
+                    if count>0:
+                        UartStr=self.ser.read(count)
+                        self.UartPrint.UartPrintStr.emit(UartStr)
+                        self.ser.flushInput()
+            except:
+                pass """
--- test.py
+++ test.py
@@ -1,17 +1,45 @@
-import serial
-import serial.tools.list_ports
- 
-port_list = list(serial.tools.list_ports.comports())
- 
-if len(port_list) <= 0:
-    print("The Serial port can't find!")
-     
-else:
-    port_list_0 =list(port_list[0])
- 
-    port_serial = port_list_0[0]
-    print(port_list_0)
- 
-    ser = serial.Serial(port_serial,9600,timeout = 60)
- 
-    print("check which port was really used >",ser.name)
+from PySide2.QtWidgets import QApplication, QTextBrowser
+from PySide2.QtUiTools import QUiLoader
+from threading import Thread
+
+from PySide2.QtCore import Signal,QObject
+
+# 自定义信号源对象类型，一定要继承自 QObject
+class MySignals(QObject):
+    # 定义一种信号，两个参数 类型分别是： QTextBrowser 和 字符串
+    text_print = Signal(QTextBrowser,str)
+    # 还可以定义其他信号
+    update_table = Signal(str)
+    
+
+class Stats:
+
+    def __init__(self):
+        self.ui = QUiLoader().load('main.ui')
+
+        # 实例化
+        self.ms = MySignals()
+
+        # 自定义信号的处理函数
+        self.ms.text_print.connect(self.printToGui)
+
+
+    def printToGui(self,fb,text):
+        fb.append(str(text))
+        fb.ensureCursorVisible()
+
+    def task1(self):
+        def threadFunc():
+            # 通过Signal 的 emit 触发执行 主线程里面的处理函数
+            # emit参数和定义Signal的数量、类型必须一致
+            self.ms.text_print.emit(self.ui.infoBox1, '输出内容')
+        
+        thread = Thread(target = threadFunc )
+        thread.start()
+
+    def task2(self):
+        def threadFunc():
+            self.ms.text_print.emit(self.ui.infoBox2, '输出内容')
+
+        thread = Thread(target=threadFunc)
+        thread.start()

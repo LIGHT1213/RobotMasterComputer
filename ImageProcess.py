3b386a9dbc0006ab0499ada337cceafdfa778529
LIGHT1213 <35650324+LIGHT1213@users.noreply.github.com>
2020-08-24 23:35:29 +0800

    Delete ImageProcess.py

--- ImageProcess.py
+++
@@ -1,7 +0,0 @@
-import cv2
-class GrayImageProcess:
-    def ImageFindContour(GrayImage,lock,self):
-        lock.acquire()
-        thresh = cv2.adaptiveThreshold(GrayImage,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,3,5)
-        GuiMember.image , GuiMember.contours , GuiMember.hierarchy = cv2.findContours ( binary , cv2.RETR_EXTERNAL , cv2.CHAIN_APPROX_SIMPLE )
-        lock.release()

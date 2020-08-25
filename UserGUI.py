2bb4de7861b2dd6a1f79375d0fbbb6fd38568564
light <pch19980807@gmail.com>
2020-08-25 11:58:09 +0800

    处理界面更改测试

--- UserGUI.py
+++ UserGUI.py
@@ -64,8 +64,9 @@ class MainGUI:
                 GrayRet,MainGUI.Grayflame=MainGUI.cap.read()
                 if RgbRet :
                     ImageLock.acquire()
-                    CapPicture2=MainGUI.Rgbflame
+                    
                     RgbCurFlame = cv2.cvtColor(MainGUI.Rgbflame, cv2.COLOR_BGR2RGB)
+                    CapPicture2=RgbCurFlame
                     heigt, width = RgbCurFlame.shape[:2]
                     RgbPic = QImage(RgbCurFlame, width, heigt, QImage.Format_RGB888)
                     RgbPic = QPixmap.fromImage(RgbPic)
@@ -93,26 +94,18 @@ class MainGUI:
             
             while True:
                 if GrayCapFlag==1:
-                    #ImageLock.acquire()
                     #thresh = cv2.adaptiveThreshold(CapPicture1,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,3,5)
                     blur = cv2.GaussianBlur(CapPicture1, (5, 5), 0)
                     ret, thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-                    ThreshShow = QImage(thresh.data,thresh.shape[1],thresh.shape[0],QImage.Format_Grayscale8)
-                    ProcessShow.ui.Binarization.setPixmap(QPixmap.fromImage(ThreshShow))
-                    #RgbGUI.ui.GrayView.setPixmap(QPixmap.fromImage(ThreshShow))
-
-                    
                     image ,contor,hes= cv2.findContours ( thresh , cv2.RETR_EXTERNAL , cv2.CHAIN_APPROX_SIMPLE )
-                    #h=image[0]
-                    cv2.drawContours(CapPicture2, contor, -1, (0, 0, 255), 3)
-                    #cv2.putText(CapPicture2, "{:.3f}".format(len ( contours )), (30, 30),cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0,255,0), 1)
-                    
-                    GrayCount = QImage(CapPicture2, CapPicture2.shape[1], CapPicture2.shape[0], QImage.Format_RGB888)
-                    GrayCount = QPixmap.fromImage(GrayCount)
-                    ProcessShow.ui.Countour.setPixmap(GrayCount)
-                    #RgbGUI.ui.GrayView.setPixmap(GrayCount)
+                    if ProcessShow.ui.ProcessCheck.isChecked():
+                        ThreshShow = QImage(thresh.data,thresh.shape[1],thresh.shape[0],QImage.Format_Grayscale8)
+                        ProcessShow.ui.Binarization.setPixmap(QPixmap.fromImage(ThreshShow))
+                        cv2.drawContours(CapPicture2, contor, -1, (255, 0, 0), 1)
+                        GrayCount = QImage(CapPicture2, CapPicture2.shape[1], CapPicture2.shape[0], QImage.Format_RGB888)
+                        GrayCount = QPixmap.fromImage(GrayCount)
+                        ProcessShow.ui.Countour.setPixmap(GrayCount)
                     GrayCapFlag=0
-                    #ImageLock.release()
                 time.sleep(0.01)
         GuiThread = Thread(target = UpdateImageShowThread)
         FindContourThread=Thread(target=ImageFindContour)
